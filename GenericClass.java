a9c3107064d772825ef4082cf22929d29a6d325f
Rutvik Dhananjay Redkar <70567390+rutvik45@users.noreply.github.com>
2023-02-17 10:59:25 +0530

    Create GenericClass.java

---
+++ GenericClass.java
@@ -0,0 +1,25 @@
+// Create a java project
+// delete module.java from src
+// create package in src
+// create class
+
+package com.sss;
+class Demo<T>
+{
+T d;
+public Demo(T data) {
+this.d =data;
+}
+public T getData() {
+return d;
+}
+}
+class Generic{
+public static void main (String[] args)
+{
+Demo<Integer> d1 = new Demo<>(100);
+Demo<String> d2 = new Demo<>("This is string");
+System.out.println("Displaying Integer data "+d1.getData());
+System.out.println("Displaying String data "+d2.getData());
+}
+}
